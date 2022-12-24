c8801b098905a72d9c6c607990841b421caafde1
DmitryE <KeDeVe@gmail.com>
2022-12-23 23:27:10 -0500

    initial

---
+++ src/main/java/kdv/spring/kdvpetclinic/KdvPetClinicApplication.java
@@ -0,0 +1,13 @@
+package kdv.spring.kdvpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdvPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdvPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kdv/spring/kdvpetclinic/KdvPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package kdv.spring.kdvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdvPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
