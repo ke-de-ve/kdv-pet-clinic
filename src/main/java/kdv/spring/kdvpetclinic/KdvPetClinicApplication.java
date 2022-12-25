46aebab31146580533380482a0a817d52d1430ff
DmitryE <KeDeVe@gmail.com>
2022-12-25 00:25:58 -0500

    03.048. Spring Pet Clinic - Multi-Module Maven Builds

--- src/main/java/kdv/spring/kdvpetclinic/KdvPetClinicApplication.java
+++ src/main/java/kdv/spring/kdvpetclinic/KdvPetClinicApplication.java
@@ -1,6 +1,5 @@
 package kdv.spring.kdvpetclinic;
 
-import kdv.spring.dependencies.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,24 +9,6 @@ public class KdvPetClinicApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(KdvPetClinicApplication.class, args);
-
-		PetController petController = (PetController) ctx.getBean("petController");
-		System.out.println(petController.whichPetIsTheBest());
-
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-		System.out.printf("I18n: %s \n", i18nController.sayHello());
-
-		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.printf("Primary bean: %s \n", myController.sayHello());
-
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println("2..." + propertyInjectedController.getGreeting());
-
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println("3..." + setterInjectedController.getGreeting());
-
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println("4..." + constructorInjectedController.getGreeting());
 	}
 
 }
