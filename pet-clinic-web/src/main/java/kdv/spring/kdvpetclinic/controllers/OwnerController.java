6465e7b724a4df6b0d02e02b1ede3a0e3a3c4972
DmitryE <KeDeVe@gmail.com>
2022-12-27 00:02:08 -0500

    05.082. Spring Pet Clinic - List Owners closes #23

--- pet-clinic-web/src/main/java/kdv/spring/kdvpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kdv/spring/kdvpetclinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package kdv.spring.kdvpetclinic.controllers;
 
+import kdv.spring.kdvpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String  listOwners() {
-        // tell Thymeleafe to search for index template
+    public String  listOwners(Model model) {
+        model.addAttribute("ownersAll", ownerService.findAll());
         return "owners/index";
     }
 }
