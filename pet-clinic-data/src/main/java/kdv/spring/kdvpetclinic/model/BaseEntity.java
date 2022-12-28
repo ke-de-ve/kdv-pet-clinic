60566d0b1a1e598c4571aa96b86f01be2cce2b17
DmitryE <KeDeVe@gmail.com>
2022-12-28 00:09:50 -0500

    Make BaseEntity and abstract class

--- pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/model/BaseEntity.java
@@ -2,7 +2,7 @@ package kdv.spring.kdvpetclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable {
+public abstract class BaseEntity implements Serializable {
 
     private Long id;
 
