a771ea0c2b7cc021b7f61249692a019077799df0
DmitryE <KeDeVe@gmail.com>
2022-12-28 22:27:58 -0500

    07.124. Spring Pet Clinic - Create Specialty Map Service closes #56

---
+++ pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package kdv.spring.kdvpetclinic.services;
+
+import kdv.spring.kdvpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package kdv.spring.kdvpetclinic.services.map;
+
+import kdv.spring.kdvpetclinic.model.Speciality;
+import kdv.spring.kdvpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends BaseMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findBiId(Long id) {
+        return super.findById(id);
+    }
+}
