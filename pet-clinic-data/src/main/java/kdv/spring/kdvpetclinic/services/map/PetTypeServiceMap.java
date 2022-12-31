36241b88b35363b035bf45e2ac2136f34c329bc0
DmitryE <KeDeVe@gmail.com>
2022-12-30 19:05:08 -0500

    08.152. Spring Pet Clinic - Add Spring Data JPA Pet Type Service closes #67

--- pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/services/map/PetTypeServiceMap.java
@@ -2,11 +2,13 @@ package kdv.spring.kdvpetclinic.services.map;
 
 import kdv.spring.kdvpetclinic.model.PetType;
 import kdv.spring.kdvpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile("map")
 public class PetTypeServiceMap  extends BaseMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
