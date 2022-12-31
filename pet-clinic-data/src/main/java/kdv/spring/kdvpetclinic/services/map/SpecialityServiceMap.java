21bcecded70e6cce7aedea7320a0dd9f5f839011
DmitryE <KeDeVe@gmail.com>
2022-12-30 19:17:17 -0500

    08.154. Spring Pet Clinic - Add Spring Data JPA Vet Speciality Service closes #69

--- pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/services/map/SpecialityServiceMap.java
+++ pet-clinic-data/src/main/java/kdv/spring/kdvpetclinic/services/map/SpecialityServiceMap.java
@@ -2,11 +2,13 @@ package kdv.spring.kdvpetclinic.services.map;
 
 import kdv.spring.kdvpetclinic.model.Speciality;
 import kdv.spring.kdvpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile("map")
 public class SpecialityServiceMap extends BaseMapService<Speciality, Long> implements SpecialityService {
 
     @Override
